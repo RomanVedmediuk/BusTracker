5f6d615bc677c71ac1ad8832879815fa8f21b8f8
Roman Vedmediuk <roman.vedmediuk@gmail.com>
2019-07-05 15:01:49 +0300

    Added core project. refactoring

--- BusTracker.Console/Program.cs
+++ BusTracker.Console/Program.cs
@@ -1,52 +1,22 @@
 ﻿namespace BusTracker.Console
 {
-    using BusTracker.GeoDataProviders.Dozor;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading;
-    using BusTracker.Contracts.Interfaces;
-    using GeoCoordinatePortable;
+    using BusTracker.Core;
 
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            ITrackingInfoProvider provider = new DozorDataProvider();
-            var routes = provider.GetRoutes();
-            var favoriteRoutes = new List<string> {"49", "39", "2"};
-            var selectedRoutes = routes.Where(r => favoriteRoutes.Contains(r.Name)).ToList();
-            var ids = selectedRoutes.Select(sr => sr.Id).ToList();
+            var core = new Core();
+            core.Start();
 
             RunUntilEscape(() =>
             {
-                var trackingData = provider.GetTrackingInformationByRouteIds(ids);
-
-                foreach (var currentTrack in trackingData)
-                {
-                    var route = selectedRoutes.FirstOrDefault(sr => sr.Id == currentTrack.RouteId);
-                    var latitude = currentTrack.GeoPosition.Latitude;
-                    var longitude = currentTrack.GeoPosition.Longitude;
-                    var nearestStation = GetClosestStation(
-                        latitude,
-                        longitude,
-                        route?.Stations);
-
-                    Console.WriteLine(
-                        $"[{route?.Name,2}] \t [{currentTrack.RegistrationNumber,10}] \t {currentTrack.GeoPosition.Course} \t {currentTrack.GeoPosition.Speed} \t [{nearestStation.Name}]");
-                }
+                core.Update();
             }, 5000);
 
-            Console.ReadLine();
-            Console.ReadKey();
-        }
-
-        private static IStationInformation GetClosestStation(double latitude, double longitude, IEnumerable<IStationInformation> trackingData)
-        {
-            var vehicleLocation = new GeoCoordinate(latitude, longitude);
-            var closestStation = trackingData.OrderBy(x => vehicleLocation.GetDistanceTo(x.Location)).FirstOrDefault();
-
-            return closestStation;
+            Console.WriteLine("Done!");
         }
 
         private static void RunUntilEscape(Action action, int threshold)
---
+++ BusTracker.Core/Core.cs
@@ -0,0 +1,56 @@
+﻿namespace BusTracker.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using BusTracker.Contracts.Interfaces;
+    using BusTracker.GeoDataProviders.Dozor;
+    using GeoCoordinatePortable;
+
+    public class Core
+    {
+        private readonly ITrackingInfoProvider provider;
+        private List<IRouteInformation> trackedRoutes;
+
+        public Core()
+        {
+            this.provider = new DozorDataProvider();
+        }
+
+        public void Start()
+        {
+            var routes = this.provider.GetRoutes();
+            var favoriteRoutes = new List<string> { "49", "39", "2" };
+            this.trackedRoutes = routes.Where(r => favoriteRoutes.Contains(r.Name)).ToList();
+
+        }
+
+        public void Update()
+        {
+            var ids = this.trackedRoutes.Select(sr => sr.Id).ToList();
+            var trackingData = this.provider.GetTrackingInformationByRouteIds(ids);
+
+            foreach (var currentTrack in trackingData)
+            {
+                var route = this.trackedRoutes.FirstOrDefault(sr => sr.Id == currentTrack.RouteId);
+                var latitude = currentTrack.GeoPosition.Latitude;
+                var longitude = currentTrack.GeoPosition.Longitude;
+                var nearestStation = GetClosestStation(
+                    latitude,
+                    longitude,
+                    route?.Stations);
+
+                Console.WriteLine(
+                    $"[{route?.Name,2}] \t [{currentTrack.RegistrationNumber,10}] \t {currentTrack.GeoPosition.Course} \t {currentTrack.GeoPosition.Speed} \t [{nearestStation.Name}]");
+            }
+        }
+
+        private static IStationInformation GetClosestStation(double latitude, double longitude, IEnumerable<IStationInformation> trackingData)
+        {
+            var vehicleLocation = new GeoCoordinate(latitude, longitude);
+            var closestStation = trackingData.OrderBy(x => vehicleLocation.GetDistanceTo(x.Location)).FirstOrDefault();
+
+            return closestStation;
+        }
+    }
+}
