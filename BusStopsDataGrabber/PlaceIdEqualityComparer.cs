5034687a66dd91e5738e69d7121932c50cee71e6
Roman Vedmediuk <roman.vedmediuk@gmail.com>
2019-07-09 17:20:01 +0300

    Added comparer by PlaceId

---
+++ BusStopsDataGrabber/PlaceIdEqualityComparer.cs
@@ -0,0 +1,38 @@
+﻿namespace BusStopsDataGrabber
+{
+    using System.Collections.Generic;
+    using Models;
+
+    public class PlaceIdEqualityComparer : IEqualityComparer<BusStop>
+    {
+        public bool Equals(BusStop x, BusStop y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(x, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(y, null))
+            {
+                return false;
+            }
+
+            if (x.GetType() != y.GetType())
+            {
+                return false;
+            }
+
+            return string.Equals(x.PlaceId, y.PlaceId);
+        }
+
+        public int GetHashCode(BusStop obj)
+        {
+            return obj.PlaceId.GetHashCode();
+        }
+    }
+}
