e3ba6351ef9704c02f92a1af48aad6b12baa5255
Roman Vedmediuk <roman.vedmediuk@gmail.com>
2019-07-09 17:20:34 +0300

    Added db context

---
+++ BusStopsDataGrabber/Context/BusStopContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusStopsDataGrabber.Context
+{
+    using Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class BusStopContext : DbContext
+    {
+        private readonly string connectionString;
+
+        public BusStopContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                this.connectionString,
+                x => x.UseNetTopologySuite());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BusStop>().HasKey(x => x.Id);
+        }
+    }
+}
